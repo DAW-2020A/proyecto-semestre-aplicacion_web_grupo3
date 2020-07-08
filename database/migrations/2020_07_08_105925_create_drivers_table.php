2935f0de6339e848dbd9db4ba8f5938c1cbfd3ed
Miguel Torres <miguetorres30@gmail.com>
2020-07-08 11:29:07 -0500

    #1 Creacion de modelo y migracion Driver

---
+++ database/migrations/2020_07_08_105925_create_drivers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',30);
+            $table->string('last_name',30);
+            $table->string('cellphone', 10);
+            $table->string('phone',8);
+            $table->string('direction',70);
+            $table->string('mail',50);
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
