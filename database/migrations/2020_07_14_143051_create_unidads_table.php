2d476d85a0e8abc1e49b47bb965c00dfb73e9bc5
Miguel Torres <miguetorres30@gmail.com>
2020-07-14 14:53:49 -0500

    #6 Creacion de modelo y migracion Unidades

---
+++ database/migrations/2020_07_14_143051_create_unidads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('placa',7);
+            $table->decimal('No_Unidad',5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidads');
+    }
+}
