e87009ac499a5384b7000be1a15fc5a6c47fdd1d
Miguel Torres <miguetorres30@gmail.com>
2020-07-29 10:48:38 -0500

    #65 Implementacion de JWT Configuracion en implementacion de JWT en el proyecto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(BusStopTableSeeder::class);
         $this->call(CategoriePlacesTableSeeder::class);
         $this->call(CooperativasTableSeeder::class);
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-
+/*
 //Rutas Bus Stop
 Route::get('bus_stops', 'BusStopController@index');
 Route::get('bus_stops/{bus_stop}', 'BusStopController@show');
@@ -59,9 +59,77 @@ Route::post('horarios', 'HorarioController@store');
 Route::put('horarios/{horarios}', 'HorarioController@update');
 Route::delete('horarios/{horarios}', 'HorarioController@delete');
 
-//Rutas InteresPoint
+//Rutas Categorie_Places
 Route::get('categorie_places', 'CategoriePlaceController@index');
 Route::get('categorie_places/{categorie_places}', 'CategoriePlaceController@show');
 Route::post('categorie_places', 'CategoriePlaceController@store');
 Route::put('categorie_places/{categorie_places}', 'CategoriePlaceController@update');
 Route::delete('categorie_places/{categorie_places}', 'CategoriePlaceController@delete');
+*/
+
+//User
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+
+//Bus Stop
+Route::get('bus_stops', 'BusStopController@index');
+Route::get('bus_stops/{bus_stop}', 'BusStopController@show');
+
+//Rutas
+Route::get('rutas', 'RutasController@index');
+Route::get('rutas/{rutas}', 'RutasController@show');
+
+//Interest Points
+Route::get('interest_points', 'InterestPointController@index');
+Route::get('interest_points/{interest_points}', 'InterestPointController@show');
+
+//Horarios
+Route::get('horarios', 'HorarioController@index');
+Route::get('horarios/{horarios}', 'HorarioController@show');
+
+//Categories Places
+Route::get('categorie_places', 'CategoriePlaceController@index');
+Route::get('categorie_places/{categorie_places}', 'CategoriePlaceController@show');
+
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    //Bus Stop
+    Route::post('bus_stops', 'BusStopController@store');
+    Route::put('bus_stops/{bus_stop}', 'BusStopController@update');
+    Route::delete('bus_stops/{bus_stop}', 'BusStopController@delete');
+
+    //Rutas
+    Route::post('rutas', 'RutasController@store');
+    Route::put('rutas/{rutas}', 'RutasController@update');
+    Route::delete('rutas/{rutas}', 'RutasController@delete');
+
+    //Driver
+    Route::get('drivers', 'DriverController@index');
+    Route::get('drivers/{drivers}', 'DriverController@show');
+    Route::post('drivers', 'DriverController@store');
+    Route::put('drivers/{drivers}', 'DriverController@update');
+    Route::delete('drivers/{drivers}', 'DriverController@delete');
+
+    //Coperativa
+    Route::get('cooperativas', 'CooperativaController@index');
+    Route::get('cooperativas/{cooperativas}', 'CooperativaController@show');
+    Route::post('cooperativas', 'CooperativaController@store');
+    Route::put('cooperativas/{cooperativas}', 'CooperativaController@update');
+    Route::delete('cooperativas/{cooperativas}', 'CooperativaController@delete');
+
+    //Interest Points
+    Route::post('interest_points', 'InterestPointController@store');
+    Route::put('interest_points/{interest_points}', 'InterestPointController@update');
+    Route::delete('interest_points/{interest_points}', 'InterestPointController@delete');
+
+    //Horarios
+    Route::post('horarios', 'HorarioController@store');
+    Route::put('horarios/{horarios}', 'HorarioController@update');
+    Route::delete('horarios/{horarios}', 'HorarioController@delete');
+
+    //Categories Places
+    Route::post('categorie_places', 'CategoriePlaceController@store');
+    Route::put('categorie_places/{categorie_places}', 'CategoriePlaceController@update');
+    Route::delete('categorie_places/{categorie_places}', 'CategoriePlaceController@delete');
+});
