e8c6644baacf614b2fbd3f8f3343fab41275d997
Miguel Torres <miguetorres30@gmail.com>
2020-09-09 20:36:55 -0500

    #79 Correcion de Cors para Frontend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use JWTAuth;
+use App\Http\Resources\User as UserResource;
 
 class UserController extends Controller
 {
