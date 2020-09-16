77d6397b78589e902c5439823e6f994afb4d3808
Freddy_Calahorrano <gfreddy.calahorrano01@epn.edu.ec>
2020-09-15 19:04:44 -0500

    #67 Creacion de Relacion Driver-Cooperativa

--- app/Driver.php
+++ app/Driver.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     protected $fillable = ['name','last_name','cellphone','phone','direction','mail','date'];
+    public function cooperativa()
+    {
+        return $this->belongsTo('App\Cooperativa');
+    }
 }
